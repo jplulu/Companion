0d7a2f3ecb64997275d8289680d7eec3d24812b1
RavindraB55 <ravindrabisram@gmail.com>
2020-02-21 16:52:23 -0500

    Added JSON and downgraded to camel case

--- src/main/java/com/lustermaniacs/companion/models/Profile.java
+++ src/main/java/com/lustermaniacs/companion/models/Profile.java
@@ -1,114 +1,121 @@
 package com.lustermaniacs.companion.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Profile {
 
     // Instance Fields
-        String first_name;
-        String last_name;
-        Byte age;
-        char gender;
-        String[] profile_pic = new String[5];
-        String location;
-        int max_distance;
-        String bio;
-        int[] survey_results = new int[5];
-        int[] sysmatched_users = new int[100];
+    String firstName;
+    String lastName;
+    Byte age;
+    char gender;
+    String[] profilePic;
+    String location;
+    int maxDistance;
+    String bio;
+    int[] surveyResults;
+    int[] sysmatchedUsers;
 
     // Constructor
-        public Profile(String first_name, String last_name, Byte age, char gender, String[] profile_pic, String location, int max_distance, String bio, int[] survey_results, int[] sysmatched_users) {
-            this.first_name = first_name;
-            this.last_name = last_name;
-            this.age = age;
-            this.gender = gender;
-            this.profile_pic = profile_pic;
-            this.location = location;
-            this.max_distance = max_distance;
-            this.bio = bio;
-            this.survey_results = survey_results;
-            this.sysmatched_users = sysmatched_users;
-        }
-
-    //Getter
-
-        public String getFirst_name() {
-            return first_name;
-        }
-
-        public String getLast_name() {
-            return last_name;
-        }
-
-        public Byte getAge() {
-            return age;
-        }
-
-        public char getGender() {
-            return gender;
-        }
-
-        public String[] getProfile_pic() {
-            return profile_pic;
-        }
-
-        public String getLocation() {
-            return location;
-        }
-
-        public int getMax_distance() {
-            return max_distance;
-        }
-
-        public String getBio() {
-            return bio;
-        }
-
-        public int[] getSurvey_results() {
-            return survey_results;
-        }
-
-        public int[] getSysmatched_users() {
-            return sysmatched_users;
-        }
-
-    // Setter
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public Profile(@JsonProperty("firstName") String firstName,
+                   @JsonProperty("lastName") String lastName,
+                   @JsonProperty("age") Byte age,
+                   @JsonProperty("gender") char gender,
+                   @JsonProperty("profilePic") String[] profilePic,
+                   @JsonProperty("location") String location,
+                   @JsonProperty("maxDistance") int maxDistance,
+                   @JsonProperty("bio") String bio,
+                   @JsonProperty("surveyResults") int[] surveyResults,
+                   @JsonProperty("sysmatchedUsers") int[] sysmatchedUsers) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.profilePic = profilePic;
+        this.location = location;
+        this.maxDistance = maxDistance;
+        this.bio = bio;
+        this.surveyResults = surveyResults;
+        this.sysmatchedUsers = sysmatchedUsers;
     }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Byte getAge() {
+        return age;
     }
 
     public void setAge(Byte age) {
         this.age = age;
     }
 
+    public char getGender() {
+        return gender;
+    }
+
     public void setGender(char gender) {
         this.gender = gender;
     }
 
-    public void setProfile_pic(String[] profile_pic) {
-        this.profile_pic = profile_pic;
+    public String[] getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String[] profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public String getLocation() {
+        return location;
     }
 
     public void setLocation(String location) {
         this.location = location;
     }
 
-    public void setMax_distance(int max_distance) {
-        this.max_distance = max_distance;
+    public int getMaxDistance() {
+        return maxDistance;
+    }
+
+    public void setMaxDistance(int maxDistance) {
+        this.maxDistance = maxDistance;
+    }
+
+    public String getBio() {
+        return bio;
     }
 
     public void setBio(String bio) {
         this.bio = bio;
     }
 
-    public void setSurvey_results(int[] survey_results) {
-        this.survey_results = survey_results;
+    public int[] getSurveyResults() {
+        return surveyResults;
     }
 
-    public void setSysmatched_users(int[] sysmatched_users) {
-        this.sysmatched_users = sysmatched_users;
+    public void setSurveyResults(int[] surveyResults) {
+        this.surveyResults = surveyResults;
+    }
+
+    public int[] getSysmatchedUsers() {
+        return sysmatchedUsers;
+    }
+
+    public void setSysmatchedUsers(int[] sysmatchedUsers) {
+        this.sysmatchedUsers = sysmatchedUsers;
     }
 }
