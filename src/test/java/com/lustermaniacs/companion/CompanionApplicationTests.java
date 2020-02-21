dbe6a9148cd0f79e6b46d59fd7a607a021e8b4d8
jplulu <lujason899@gmail.com>
2020-02-21 13:20:13 -0500

    Initial commit

---
+++ src/test/java/com/lustermaniacs/companion/CompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lustermaniacs.companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
