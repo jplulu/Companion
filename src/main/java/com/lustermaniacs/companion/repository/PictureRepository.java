f1223a177220dcf1328403d1364b1f3e96bcb6e3
RavindraB55 <ravindrabisram@gmail.com>
2020-04-17 16:49:55 -0400

    DO NOT MERGE OR PULL THIS, trying to resolve conflict

--- src/main/java/com/lustermaniacs/companion/repository/PictureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lustermaniacs.companion.repository;
-
-import com.lustermaniacs.companion.models.Picture;
-import com.lustermaniacs.companion.models.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PictureRepository extends JpaRepository<Picture, Integer> {
-    void deleteByNameAndProfile(String name, Profile profile);
-}
