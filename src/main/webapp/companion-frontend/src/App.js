a2e67db4d2b8583c1845b819e893013b61220020
jplulu <lujason899@gmail.com>
2020-04-18 12:56:08 -0400

    Redux added

--- src/main/webapp/companion-frontend/src/App.js
+++ src/main/webapp/companion-frontend/src/App.js
@@ -3,58 +3,29 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css'
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import themeFile from './util/theme'
+import jwtDecode from 'jwt-decode'
 // Components
 import NavigationBar from "./components/NavigationBar";
+import AuthRoute from './util/AuthRoute'
 // Pages
 import home from './pages/home'
 import login from './pages/login'
 import signup from './pages/signup'
 
-const theme = createMuiTheme({
-    palette: {
-        primary: {
-            light: '#33c9dc',
-            main: '#00bcd4',
-            dark: '#008394',
-            contrastText: '#fff'
-        },
-        secondary: {
-            light: '#ff6333',
-            main: '#ff3d00',
-            dark: '#b22a00',
-            contrastText: '#fff'
-        }
-    },
-    typography: {
-        useNextVariants: true
-    },
-    spreadThis: {
-        form:{
-            textAlign: 'center'
-        },
-        image: {
-            margin: '20px auto 20px auto'
-        },
-        pageTitle: {
-            margin: '10px auto 10px auto'
-        },
-        textField: {
-            margin: '10px auto 10px auto'
-        },
-        button: {
-            marginTop: 20,
-            position: 'relative'
-        },
-        customError: {
-            color: 'red',
-            fontSize: '0.8rem',
-            marginTop: 10
-        },
-        loadSpinner: {
-            position: 'absolute'
-        }
+const theme = createMuiTheme(themeFile);
+
+let authenticated;
+const token = localStorage.jwtToken;
+if(token) {
+    const decodeToken = jwtDecode(token);
+    if(decodeToken.exp * 1000 < Date.now()) {
+        window.location.href = 'http://localhost:3000/login';
+        authenticated = false;
+    } else {
+        authenticated = true;
     }
-});
+}
 
 function App() {
   return (
@@ -65,8 +36,8 @@ function App() {
                 <div className="container">
                     <Switch>
                         <Route path="/" exact component={home}/>
-                        <Route path="/login" exact component={login}/>
-                        <Route path="/signup" exact component={signup}/>
+                        <AuthRoute path="/login" exact component={login} authenticated={authenticated}/>
+                        <AuthRoute path="/signup" exact component={signup} authenticated={authenticated}/>
                     </Switch>
                 </div>
             </Router>
