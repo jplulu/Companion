5fa3e7fa2d92e9820131477047325646303a3571
RavindraB55 <ravindrabisram@gmail.com>
2020-04-19 16:55:48 -0400

    DO NOT MERGE OR PULL THIS, trying to resolve conflict

---
+++ src/main/webapp/companion-frontend/src/pages/survey.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class survey extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+
+    }
+
+    render(){
+        return(
+
+        )
+    }
+
+
+}
