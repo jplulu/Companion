c97bc2b4f506b0dbf350bfa5a8ec19eb6e9b4ff9
jplulu <lujason899@gmail.com>
2020-04-18 20:11:57 -0400

    Added dynamic NavigationBar

--- src/main/webapp/companion-frontend/src/components/NavigationBar.js
+++ src/main/webapp/companion-frontend/src/components/NavigationBar.js
@@ -1,5 +1,7 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
 
 // MUI
 import AppBar from '@material-ui/core/AppBar';
@@ -8,16 +10,36 @@ import Button from '@material-ui/core/Button';
 
 class NavigationBar extends Component {
     render() {
+        const {authenticated} = this.props;
         return(
             <AppBar>
                 <Toolbar className="nav-container">
-                    <Button color="inherit" component={Link} to="/login">Login</Button>
-                    <Button color="inherit" component={Link} to="/">Home</Button>
-                    <Button color="inherit" component={Link} to="/signup">Sign up</Button>
+                    {authenticated ? (
+                        <Fragment>
+                            <Button color="inherit" component={Link} to="/">Home</Button>
+                            <Button color="inherit" component={Link} to={`/user/${this.props.username}`}>My Profile</Button>
+                        </Fragment>
+                    ) : (
+                        <Fragment>
+                            <Button color="inherit" component={Link} to="/login">Login</Button>
+                            <Button color="inherit" component={Link} to="/">Home</Button>
+                            <Button color="inherit" component={Link} to="/signup">Sign up</Button>
+                        </Fragment>
+                    )}
                 </Toolbar>
             </AppBar>
         )
     }
 }
 
-export default NavigationBar;
+NavigationBar.propTypes = {
+    authenticated: PropTypes.bool.isRequired,
+    username: PropTypes.string.isRequired
+};
+
+const mapStateToProps = state => ({
+    username: state.user.username,
+    authenticated: state.user.authenticated
+});
+
+export default connect(mapStateToProps)(NavigationBar);
